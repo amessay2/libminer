0ac78e1f498abbc0c0d90d6eb730fedbe83da648
Endegena Desta <en68923@hhrinstitute.org>
2023-09-18 15:52:46 -0500

    idk

--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -1,3 +1,20 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("lib_summary returns expected results", {
+  res = lib_summary()
+  expect_s3_class(res, "data.frame")
+  expect_equal(ncol(res), 2)
+  expect_equal(names(res), c("library", "n_packages"))
+  expect_type(res$library, "character")
+  expect_type(res$n_packages, "integer")
+})
+
+test_that("lib_summary fails appropriately", {
+  expect_error(lib_summary("foo"), "sizes must be logical")
+})
+
+test_that("sizes argument works", {
+  res = lib_summary(sizes = TRUE)
+  expect_s3_class(res, "data.frame")
+  expect_equal(ncol(res), 3)
+  expect_equal(names(res), c("library", "n_packages", "lib_size"))
+  expect_type(res$lib_size, "double")
 })
